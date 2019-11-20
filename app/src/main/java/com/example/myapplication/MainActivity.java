d4f70794ecc6a1205987e2ba0ea2f3e6bdbd204a
Meriliana <cmeriliana@yahoo.com>
2019-11-20 10:06:15 +0800

    Commit 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.myapplication;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.table_layout);
+        viewPager = (ViewPager) findViewById(R.id.view_paper);
+        ViewPageAdapter adapter = new ViewPageAdapter(getSupportFragmentManager());
+
+        adapter.AddFragments(new FragmentDashboard(), "Dashboard");
+        adapter.AddFragments(new FragmentAddEvent(), "AddEvent");
+        adapter.AddFragments(new FragmentNotification(), "Notification");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
